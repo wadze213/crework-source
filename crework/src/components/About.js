ba4b8f7a264e0ecff297102a9461fe8957b813bb
wadze213 <94960172+wadze213@users.noreply.github.com>
2022-10-20 17:16:30 +0800

    Fixing css source

--- crework/src/components/About.js
+++ crework/src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles.css'
+import './component-styles/styles.css'
 
 const About = () => {
   return (
--- crework/src/components/How.js
+++ crework/src/components/How.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import './styles.css'
-import './colors.css'
+import './component-styles/styles.css'
+import './component-styles/colors.css'
 
 const How = () => {
   return (
