2b4b8308a0964c54080968b35bd7d206a6ffb46a
wadze213 <94960172+wadze213@users.noreply.github.com>
2022-10-22 14:10:57 +0800

    Added landing by Danil

--- crework/src/components/Landing.js
+++ crework/src/components/Landing.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './component-styles/styles.css';
+import './component-styles/colors.css';
+import Navigation from './Navigation';
 
 const Landing = () => {
   return (
     <section>
-      Landing
+      <Navigation/>
+      <div className='landing-container'>
+        <h1>A <span className='chunky-underline-blue'>DYNAMIC</span> AGENCY <br></br> FOR YOUR DYNAMIC <span className='chunky-underline-pink'>NEEDS</span></h1>
+        <p className='landing-p'>What do you need ?</p>
+        <div className='button-container'>
+          <button className='btn-outline'>Work to do</button>
+          <button className='btn-black'>Work done</button>
+        </div>
+      </div>
     </section>
   )
 }
