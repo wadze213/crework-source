cd67fd45c0a947fd232e310fc2e89a95c692c0f2
wadze213 <94960172+wadze213@users.noreply.github.com>
2022-10-22 15:22:55 +0800

    Adding Join by Glenn

--- crework/src/components/Join.js
+++ crework/src/components/Join.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import './component-styles/styles.css'
+import talent from '../images/talent_3.png'
+import '../components/component-styles/join.css'
 
 const Join = () => {
   return (
-    <section>
-        Join us
+    <section className='join'>
+      <div className='joinContainer'>
+          <div>
+          <h1 className='joinTitle'>FOR TALENTS</h1>
+          <img src={talent} alt=""></img>
+          </div>
+          <div >
+            <div className='joinBox'>
+              <div className='joinOrange '>
+                <h2>WORK LIFE BALANCE</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem </p>
+                
+              </div>
+              <div className='joinPink'>
+                  <h2>CURATED WORK</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem </p>
+                
+              </div>
+              <div className='joinBlue '>
+                  <h2>ALWAYS ON TIME</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem</p>
+                  
+              </div>
+            </div>
+          </div>
+      </div>
     </section>
   )
 }
