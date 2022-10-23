5576d883459bbfdd80e49a5904ecfe2aa9da5624
wadze213 <94960172+wadze213@users.noreply.github.com>
2022-10-23 21:52:51 +0800

    Adding join us form by Glenn -tobeupdated

--- crework/src/components/Form .jsx
+++ crework/src/components/Form .jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect} from 'react'
 import '../components/component-styles/styles.css'
-import FormInput from './FormInput'
+
 
 
 
@@ -12,100 +12,78 @@ const Form = () => {
 
   const [values,setValues]=useState([Input])
 
-
-useEffect(() => {
-  localStorage.setItem("SignupForm",JSON.stringify(values))//chores-dependency
-
-},[values])
-
-
-
-
-const input =[
-  {
-    id:1,
-    name:"firstname",
-    type:"text",
-    placeholder:"Enter First name",
-    label:"First name",
-    errorMessage:"Please enter your First name",
-    required:true,
-  },
-  {
-    id:2,
-    name:"lastname",
-    type:"text",
-    placeholder:"Enter Last name",
-    label:"Last name",
-    errorMessage:"Please enter your Last name",
-    required:true,
-  },
-  {
-    id:3,
-    name:"email",
-    type:"email",
-    placeholder:"ex:myname@example.com",
-    errorMessage:"It should be a valid email address!",
-    label:"Email",
-    required:true,
-  },
-  {
-    id:4,
-    name:"Position",
-    type:"text",
-    placeholder:"ex:Graphic Designer",
-    label:"Position",
-    errorMessage:"Please enter your Position",
-    required:true,
-  },
-  {
-    id:5,
-    name:"Bio",
-    type:"text",
-    placeholder:"Enter your bio",
-    label:"Bio",
-    errorMessage:"Please enter your Bio",
-    required:true,
-   
-  },
- 
-
- 
-];
-
+  const input =[
+    {
+      id:1,
+      name:"firstname",
+      type:"text",
+      placeholder:"Enter First name",
+      label:"First name",
+      errorMessage:"Please enter your First name",
+      required:true,
+    },
+    {
+      id:2,
+      name:"lastname",
+      type:"text",
+      placeholder:"Enter Last name",
+      label:"Last name",
+      errorMessage:"Please enter your Last name",
+      required:true,
+    },
+    {
+      id:3,
+      name:"email",
+      type:"email",
+      placeholder:"ex:myname@example.com",
+      errorMessage:"It should be a valid email address!",
+      label:"Email",
+      required:true,
+    },
+    {
+      id:4,
+      name:"Position",
+      type:"text",
+      placeholder:"ex:Graphic Designer",
+      label:"Position",
+      errorMessage:"Please enter your Position",
+      required:true,
+    },
+    {
+      id:5,
+      name:"Bio",
+      type:"text",
+      placeholder:"Enter your bio",
+      label:"Bio",
+      errorMessage:"Please enter your Bio",
+      required:true,
+     
+    },
+  ]
 
 const handleSubmit = (e)=>{
-  e.preventDefault();
-  
-  }
-  const onChange = (e) =>{
+    e.preventDefault();
+    let info = {
+      fName: document.getElementsByName("firstname").value
+    }
+    console.log(info);
     setValues({...values,[e.target.name]:e.target.value});
-  };
-
-
-
-
-
-  
-
-
-
-
+    }
+  useEffect(() => {
+    localStorage.setItem("SignupForm",JSON.stringify(values))//chores-dependency
+  },[values])
 
   return (
     <div className='forms center-section'>
       <form  onSubmit={handleSubmit}>
       <h1>Sign up</h1>
-        {input.map((input) => (
-            <FormInput key={input.id}{...input}
-            value={values[input.name]} 
-            onChange={onChange} />
-        ))}
+
           
             
           <button type='submit' >Create my account</button>
             </form>
     </div>
+
   )
 }
 
