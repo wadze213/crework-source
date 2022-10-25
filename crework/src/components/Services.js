664fac4d7f416177af0373fd2cff9e5004195c5c
wadze213 <94960172+wadze213@users.noreply.github.com>
2022-10-25 21:56:35 +0800

    Updating TallentPoolk page with component and layout

--- crework/src/components/Services.js
+++ crework/src/components/Services.js
@@ -10,21 +10,23 @@ const Services = () => {
    
             <h2>WHAT <span className='chunky-underline-orange'>CAN</span> WE DO</h2>
             
-            <p className='services-content'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
+            <p className='services-content'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem.</p>
         </div>
         <div class="services-card-container">
           <div class="servicescard">
             <div class="servicescard-details">
               <div className='servicescard-img'></div>
-              <p class="servicestext-title">Branding & Design</p>
-              <p class="servicestext-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum .</p>
+              <div>
+                <p class="servicestext-title">Branding & Design</p>
+                <p class="servicestext-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum .</p>
+              </div>
             </div>
             
           </div>
           <div class="servicescard">
             <div class="servicescard-details">
               <div className='servicescard-img2'></div>
-              <p class="servicestext-title">Full Stack Web Dev.</p>
+              <p class="servicestext-title">Web Dev.</p>
               <p class="servicestext-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum .</p>
             </div>
             
@@ -42,11 +44,7 @@ const Services = () => {
         
         
         
-          <button class="cssbuttons-io-button"> Let's work on your Idea
-  <div class="icon">
-    <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
-  </div>
-</button>
+          <button class="btn-black"> Let's work on your Idea</button>
      
     </section>
   )
--- crework/src/components/TalentPool.js
+++ crework/src/components/TalentPool.js
@@ -1,50 +1,18 @@
 import React from 'react'
 import TalentCard from './TalentCard'
-import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-
-function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
 
 const TalentPool = () => {
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
   return (
     <div className='talentpool-container'>
+        <div className='talent-pool-title'>
         <p>A crew of the best remote talent around.</p>
-        <h2>Connecting oustanding <br></br> remote talent with the most <br></br> innovative companies</h2>
+        <h2>CONNECTING OUTSTANDING <br></br> <span className='chunky-underline-blue'>REMOTE TALENT</span> WITH THE MOST<br></br> INNOVATIVE <span className='chunky-underline-pink'>COMPANIES</span></h2>
+        </div>
+        
 
         <div className='featured-talent-container'>
-            <h3>Our talent pool</h3>
+            <h3>FEATURED TALENTS</h3>
             <div style={{padding: "50px"}}></div>
             <TalentCard/>
         </div>
