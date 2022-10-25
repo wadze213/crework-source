b9952f87d8228df9e74787edede614c292ca4c0c
wadze213 <94960172+wadze213@users.noreply.github.com>
2022-10-25 16:17:49 +0800

    WIP-push

--- crework/src/components/TalentCard.js
+++ crework/src/components/TalentCard.js
@@ -1,13 +1,65 @@
 import React from 'react'
 //Sample data to work with
 import SampleTalents from './sampleTalents'
+import './component-styles/talentcard.css'
+import './component-styles/services.css'
+import { useState,useRef } from 'react'
+
+
+
+
+const checkAvailable =(available)=> {
+  let availBox = document.getElementById("availBox")
+  if(available === true){
+    return("Available to work")
+  }else{
+    availBox.classList.add("grey")
+    availBox.classList.remove("green") 
+    return("unavailbale")
+    
+  }
+}
+
 
 const TalentCard = () => {
+
+  const checkAvailable =(available)=> {
+    if(available === true){
+      return(
+        <div className="availability">
+          <span className="box green"></span>
+          <p>Available to work</p>
+        </div>
+      )
+    }else{
+      return(
+        <div className="availability">
+          <span className="box grey"></span>
+          <p>Not available</p>
+        </div>
+      )
+    }
+  }
+
+  let [talentList, setTalentList] = useState(SampleTalents);
+  console.log(talentList)
   return (
-    <div>
-      
+    <div className='talent-card-container'>
+      {talentList.map((talent) => {
+        return(<div className="talent-card">
+        <div className="card-img"></div>
+        <div className="card-info">
+          <h3>{talent.fName + " " + talent.lName}</h3>
+          <h5>{talent.position}</h5>
+          <p>{talent.bio}</p>
+        </div>
+        {checkAvailable(talent.avilable)}
+      </div>)
+      })}
     </div>
+    
   )
 }
 
 export default TalentCard
+
--- crework/src/components/TalentPool.js
+++ crework/src/components/TalentPool.js
@@ -1,11 +1,54 @@
 import React from 'react'
 import TalentCard from './TalentCard'
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
 
 const TalentPool = () => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
   return (
-    <div>
-        Talentpool.js
-      <TalentCard/>
+    <div className='talentpool-container'>
+        <p>A crew of the best remote talent around.</p>
+        <h2>Connecting oustanding <br></br> remote talent with the most <br></br> innovative companies</h2>
+
+        <div className='featured-talent-container'>
+            <h3>Our talent pool</h3>
+            <div style={{padding: "50px"}}></div>
+            <TalentCard/>
+        </div>
+      
     </div>
   )
 }
