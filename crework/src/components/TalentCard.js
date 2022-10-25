5ec3ef00dcfa76b20707281a05dd8ce06fc6a5ea
wadze213 <94960172+wadze213@users.noreply.github.com>
2022-10-25 21:54:50 +0800

    Adding Talent card component by Glenn

--- crework/src/components/TalentCard.js
+++ crework/src/components/TalentCard.js
@@ -3,25 +3,18 @@ import React from 'react'
 import SampleTalents from './sampleTalents'
 import './component-styles/talentcard.css'
 import './component-styles/services.css'
-import { useState,useRef } from 'react'
+import { useState } from 'react'
 
 
 
 
-const checkAvailable =(available)=> {
-  let availBox = document.getElementById("availBox")
-  if(available === true){
-    return("Available to work")
-  }else{
-    availBox.classList.add("grey")
-    availBox.classList.remove("green") 
-    return("unavailbale")
-    
-  }
-}
 
 
 const TalentCard = () => {
+
+  let [talentList, setTalentList] = useState(SampleTalents);
+
+
 
   const checkAvailable =(available)=> {
     if(available === true){
@@ -41,16 +34,76 @@ const TalentCard = () => {
     }
   }
 
-  let [talentList, setTalentList] = useState(SampleTalents);
+
+
+const PositionColor =(item)=> {
+  if(item.positionType === "Design"){
+    return(
+      <div className="positionColor">
+         <span className="bluetext"> {item.position} </span>
+      </div>
+    )
+  }else if (item.positionType === "Dev"){
+    return(
+      <div className="positionColor">
+        <span className="orangetext">{item.position} </span>
+      </div>
+    )
+  }else{
+    return(
+      <div className="positionColor">
+        <span className="pinktext">{item.position} </span>
+      </div>
+    )
+  }
+}
+
+const borderColor =(item) =>{
+  if(item.positionType === "Design"){
+    return(
+      `talent-card blueBorder`
+    )
+  }else if (item.positionType === "Dev"){
+    return(
+      `talent-card orangeBorder`
+    )
+  }else{
+    return(
+      `talent-card pinkBorder`
+    )
+  }
+}
+
+
+const borderImgColor =(item) =>{
+  if(item.positionType === "Design"){
+    return(
+      `card-img blueimgBorder`
+    )
+  }else if (item.positionType === "Dev"){
+    return(
+      `card-img orangeimgBorder`
+    )
+  }else{
+    return(
+      `card-img pinkimgBorder`
+    )
+  }
+}
+
+
+
+ 
   console.log(talentList)
   return (
     <div className='talent-card-container'>
       {talentList.map((talent) => {
-        return(<div className="talent-card">
-        <div className="card-img"></div>
+        return(<div className={borderColor(talent)}>
+       
+        <div className={borderImgColor(talent)}></div>
         <div className="card-info">
           <h3>{talent.fName + " " + talent.lName}</h3>
-          <h5>{talent.position}</h5>
+          {PositionColor(talent)}
           <p>{talent.bio}</p>
         </div>
         {checkAvailable(talent.avilable)}
