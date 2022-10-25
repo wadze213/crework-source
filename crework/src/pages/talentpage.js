ebbf3650b1f51a79ba669a785f07ea11f28d7b47
wadze213 <94960172+wadze213@users.noreply.github.com>
2022-10-25 21:55:12 +0800

    Adding Talent page layout by Danil

--- crework/src/pages/talentpage.js
+++ crework/src/pages/talentpage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import TalentPool from '../components/TalentPool'
+import Navigation from '../components/Navigation'
 
 const TalentPage = () => {
   return (
     <div>
         <TalentPool/>
+        <Navigation/>
     </div>
   )
 }
